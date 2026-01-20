e68f4621ab5b8a5c68e9a1e33883a5903141f2d1
Josh Bailey <josh@vandervecken.com>
2026-01-21 09:42:21 +1300

    new orin 2.3.

--- torchserve/install-orin-torchserve.sh
+++ torchserve/install-orin-torchserve.sh
@@ -7,17 +7,13 @@ apt-get update && \
     python3-pip \
     openjdk-17-jdk \
     wget \
-    numactl
-  apt-get purge -y opencv-python && \
-  pip install "opencv-python<4.8.0.76" && \
-  echo /usr/local/lib/python3.8/dist-packages/opencv_python.libs > /etc/ld.so.conf.d/opencv.conf && \
-  ldconfig && \
+    numactl && \
   $(dirname $0)/clone-torchserve.sh && \
   cd serve && \
   pip install -r requirements/common.txt && \
   pip install -r requirements/common_gpu.txt && \
   pip install -r requirements/torch_common.txt && \
-  pip install . && \
+  pip install .  && \
   cd .. && \
   rm -rf serve && \
   $(dirname $0)/install-common.sh
