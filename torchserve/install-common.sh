b85fb05912846648d828b1f60af1956ab3ba3e13
josh <josh@orinnano.finf>
2026-01-20 20:48:14 +0000

    Orin/pytorch2.3.

--- torchserve/install-common.sh
+++ torchserve/install-common.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apt-get update && apt-get install -yq curl wget jq
-pip install $* opencv-python pandas py-cpuinfo scipy seaborn setuptools thop
+pip install $* opencv-python pandas py-cpuinfo scipy seaborn setuptools thop nvgpu "numpy<2.0"
